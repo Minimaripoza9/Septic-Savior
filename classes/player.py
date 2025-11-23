ed3aba82fcc89fc608837e2cf001b7b74b03fb81
EstebanAOcasio <esteban.ocasio2@upr.edu>
2025-11-22 23:12:49 -0400

    Updated player.py
    now uses vectors for player movement calculation. makes for smoother controls.

--- classes/player.py
+++ classes/player.py
@@ -1,23 +1,38 @@
 import pygame
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, item1, item2, health: int = 100):
+    def __init__(self, starting_pos = (0, 0), move_speed = 1, starting_items: list = [], starting_health: int = 3):
         super().__init__()
-        self.image = pygame.image.load("placehold_p.png").convert()
+        self.image = pygame.image.load("assets/player.png").convert()
         self.image.set_colorkey((0, 0, 0))
         self.rect = self.image.get_rect()
-        self.MAXFALL = 6
-        self.velocity = 0
-        self.MAXHP = 100
-        self.hp = health
+        self.rect.topleft = starting_pos
 
-    def move(self):
+        self.direction_x = True #0 for left, 1 for right
+        self.direction_y = True #0 for up, 1 for down
+        self.velocity = pygame.math.Vector2(0, 0)
+        self.acceleration = pygame.math.Vector2(0,0)
+        self.speed = 10
+
+        self.MAXHP = 3
+        self.hp = starting_health
+        self.items = starting_items
+
+    def update(self, surface_friction: float = 0.2):
+
+        self.acceleration = pygame.math.Vector2(0,0)
+        current_acceleration = self.speed/10
+
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
-            self.rect.x -= self.velocity
+            self.acceleration.x = -current_acceleration
         if keys[pygame.K_RIGHT]:
-            self.rect.x += self.velocity
+            self.acceleration.x = current_acceleration
         if keys[pygame.K_UP]:
-            self.rect.y -= self.velocity
+            self.acceleration.y = -current_acceleration
         if keys[pygame.K_DOWN]:
-            self.rect.y += self.velocity
+            self.acceleration.y = current_acceleration
+
+        self.acceleration -= self.velocity * surface_friction
+        self.velocity += self.acceleration
+        self.rect.topleft += self.velocity + (0.5 * self.acceleration)
